eee53e478fab834899a2a1988e8f43e3653f26f7
Ian Ibbotson <i.ibbotson@shu.ac.uk>
2012-12-17 18:02:25 +0000

    updated

--- grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
+++ grails-app/domain/uk/ac/shu/webarch/eregister/Instructor.groovy
@@ -5,6 +5,14 @@ class Instructor {
   String name
   String staffNumber
 
-    static constraints = {
-    }
+  static hasMany = [
+    classes_taught_by_this_instructor:RegClass
+  ]
+
+  static mappedBy = [
+    classes_taught_by_this_instructor:'class_instructor'
+  ]
+
+  static constraints = {
+  }
 }
